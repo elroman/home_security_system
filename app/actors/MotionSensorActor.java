97a3396546a3132ff01fc154d299e68708aef5a2
roman <elroman4@gmail.com>
2017-10-22 21:26:45 +0300

    added pir sensor

--- app/actors/MotionSensorActor.java
+++ app/actors/MotionSensorActor.java
@@ -1,5 +1,12 @@
 package actors;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.RaspiPin;
+
+import java.time.Duration;
+
 import actors.cmd.ActivationCmd;
 import akka.actor.AbstractActor;
 import akka.event.LoggingReceive;
@@ -12,12 +19,19 @@ public class MotionSensorActor
     extends AbstractActor {
 
     private Boolean active;
+    private final GpioController gpio;
+    private final GpioPinDigitalInput input;
 
     @Override
     public PartialFunction<Object, BoxedUnit> receive() {
         return LoggingReceive.create(ReceiveBuilder
                                          .match(ActivationCmd.class, this::setActivation)
                                          .build(), getContext());
+    }
+
+    public MotionSensorActor() {
+        gpio = GpioFactory.getInstance();
+        input = gpio.provisionDigitalInputPin(RaspiPin.GPIO_11, "MyInput");
     }
 
     private void setActivation(ActivationCmd cmd) {
@@ -29,5 +43,15 @@ public class MotionSensorActor
 
     private void readMotionSensor() {
 
+        while (active) {
+            if (input.getState().isHigh()) {
+                Logger.debug("== Move detected!!!!!");
+            }
+            try {
+                Thread.sleep(Duration.ofSeconds(1).toMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
