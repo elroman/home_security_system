9c3b130c46f4e1a8bce50739be0fd22e7a420969
roman <elroman4@gmail.com>
2017-11-05 16:12:12 +0200

    added folder for photo. refactored code

--- app/actors/MotionSensorActor.java
+++ app/actors/MotionSensorActor.java
@@ -7,7 +7,7 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.RaspiPin;
 
-import java.time.Duration;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Named;
@@ -22,12 +22,13 @@ import akka.event.LoggingReceive;
 import akka.japi.pf.ReceiveBuilder;
 import play.Logger;
 import scala.PartialFunction;
+import scala.concurrent.duration.Duration;
 import scala.runtime.BoxedUnit;
+import service.DateUtil;
 
 public class MotionSensorActor
     extends AbstractActor {
 
-    private volatile Boolean active;
     private GpioController gpio;
     private GpioPinDigitalInput input;
     private Cancellable trackerScheduler;
@@ -58,50 +59,42 @@ public class MotionSensorActor
         }
     }
 
-    private void setActivation(ActivationCmd cmd) {
-        Logger.debug("MotionSensorActor: setActivation:  " + cmd);
-
-        active = cmd.isActive();
-        if (active) {
-            self().tell(new ReadMotionSensorCmd(), ActorRef.noSender());
-        }
-    }
+    //    private void setActivation(ActivationCmd cmd) {
+    //        Logger.debug("MotionSensorActor: setActivation:  " + cmd);
+    //
+    //        active = cmd.isActive();
+    //
+    //        if (active) {
+    //            self().tell(new ReadMotionSensorCmd(), ActorRef.noSender());
+    //        }
+    //    }
 
     private void readMotionSensor(ReadMotionSensorCmd cmd) {
-        Logger.debug("== Move detected!!!!!");
-        while (active) {
-           /* if (input.getState().isHigh()) {
-                Logger.debug("== Move detected!!!!!");
-                securitySystem.tell(new DetectedMoveEvt(), self());
-            }
-            try {
-                Thread.sleep(Duration.ofSeconds(1).toMillis());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }*/
+        if (input.getState().isHigh()) {
+            Logger.debug("Move detected:", DateUtil.getStringFromDateTime(new Date()));
+            securitySystem.tell(new DetectedMoveEvt(), self());
         }
     }
 
-
-
-    /*private void setActivation(ActivationCmd cmd) {
-        Logger.debug(" == setActivation: ", cmd.isActive());
+    private void setActivation(ActivationCmd cmd) {
         if (cmd.isActive()) {
+            Logger.debug("Motion sensor was activated !");
             if (trackerScheduler.isCancelled()) {
                 trackerScheduler = getContext().system().scheduler().schedule(
                     Duration.Zero(),
-                    Duration.create(30, TimeUnit.SECONDS),
+                    Duration.create(1, TimeUnit.SECONDS),
                     self(),
                     new ReadMotionSensorCmd(),
                     getContext().dispatcher(),
                     null
                 );
             } else {
+                Logger.debug("Motion sensor was deactivated!");
                 trackerScheduler.cancel();
             }
         }
     }
-
+/*
     private void startReadMotionSensor(ReadMotionSensorCmd cmd) {
 
         for (int i = 0; i < 10; i++) {
